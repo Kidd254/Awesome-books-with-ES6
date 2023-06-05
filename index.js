6f267bd009f2f62bcba42c7416079b23a94172c6
Kidd254 <lorenkioko@gmail.com>
2023-06-05 20:20:11 +0300

    correct addbook function

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const form = document.querySelector('#form-book');
+
 import { pDate, list, addNew, contact, booksSection, formSection, contactSection, showForm, showContact, showBooks } from './modules/menu.js';
 import { DateTime } from './modules/Luxon.js';
 
@@ -14,17 +12,17 @@ list.addEventListener('click', showBooks);
 addNew.addEventListener('click', showForm);
 contact.addEventListener('click', showContact);
 
-import {mainContainer} from './modules/books.js'
-import Books from './modules/books.js';
+//import {mainContainer} from './modules/books.js'
 
 
-const books = new Books();
+
+import {books} from './modules/addBook.js'
 
 // Access the methods of the Books class
 books.createBook(books);
 books.displayBooks();
 
 // A function that is called when the user clicks the submit button
-import { addBook } from './modules/addBook.js';
+import { addBook, form } from './modules/addBook.js';
 form.addEventListener('submit', addBook);
 
--- modules/addBook.js
+++ modules/addBook.js
@@ -1,3 +1,8 @@
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+export const form = document.querySelector('#form-book');
+import Books from './books.js';
+export const books = new Books();
 export function addBook(event) {
   // If the inputs have values, then the form is submitted
   if (!titleInput.validity.valueMissing && !authorInput.validity.valueMissing) {
